1f12e4f5ba047ee0e18d869107d7a8a311298dc9
tbz <2070957476>
2025-05-14 19:05:00 +0800

    add theme toggle

---
+++ components/themeToggle.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className="p-2 w-10 h-10 rounded-full cursor-pointer bg-gray-200 hover:bg-gray-300 dark:bg-gray-800 hover:dark:bg-gray-700"
+    >
+      {theme === "dark" ? (
+        <Sun className="w-6 h-6" />
+      ) : (
+        <Moon className="w-6 h-6" />
+      )}
+    </button>
+  );
+}
