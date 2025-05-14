c8b0186e81e2f223f77b1aae197620431df3c0f4
tbz <2070957476>
2025-05-14 16:56:36 +0800

    change project structure

---
+++ app/(root)/(home)/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <nav className="flex justify-between items-center p-6">
+      <h1 className="text-6xl font-bold">HOME</h1>
+    </nav>
+  );
+}
---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "./header";
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+}
