17a833931a82324a986610d53a3608e9bdef893c
tbz <2070957476>
2025-05-15 14:42:33 +0800

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "eslint-config-prettier"),
 ];
 
 export default eslintConfig;
