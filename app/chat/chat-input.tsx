18db6a97812d6a7d55b8b6b6792c35d77cc4d30c
yu_ran <2104208169@qq.com>
2025-05-25 18:50:55 +0800

    improve icon

--- app/chat/chat-input.tsx
+++ app/chat/chat-input.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SendIcon, Loader2 } from "lucide-react";
+import { Loader2 } from "lucide-react";
 import { useRef, useEffect, useState } from "react";
 import RenderFormPending from "./render-form-pending";
 
@@ -72,7 +72,9 @@ export default function ChatInput({
         >
           <RenderFormPending
             pendingNode={<Loader2 className="h-4 w-4 animate-spin" />}
-            notPendingNode={<SendIcon className="h-4 w-4 mt-0.5 transform" />}
+            notPendingNode={
+              <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M7 16c-.595 0-1.077-.462-1.077-1.032V1.032C5.923.462 6.405 0 7 0s1.077.462 1.077 1.032v13.936C8.077 15.538 7.595 16 7 16z" fill="currentColor"></path><path fillRule="evenodd" clipRule="evenodd" d="M.315 7.44a1.002 1.002 0 0 1 0-1.46L6.238.302a1.11 1.11 0 0 1 1.523 0c.421.403.421 1.057 0 1.46L1.838 7.44a1.11 1.11 0 0 1-1.523 0z" fill="currentColor"></path><path fillRule="evenodd" clipRule="evenodd" d="M13.685 7.44a1.11 1.11 0 0 1-1.523 0L6.238 1.762a1.002 1.002 0 0 1 0-1.46 1.11 1.11 0 0 1 1.523 0l5.924 5.678c.42.403.42 1.056 0 1.46z" fill="currentColor"></path></svg>
+            }
           />
         </button>
       </div>
