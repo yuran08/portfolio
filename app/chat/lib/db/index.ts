efa0e64fdc2790f946d8c283c245593c6c3e49b8
tbz <2070957476>
2025-09-24 16:26:33 +0800

    fix: env change from vercel

--- app/chat/lib/db/index.ts
+++ app/chat/lib/db/index.ts
@@ -3,7 +3,7 @@ import * as schema from "./schema";
 import * as relations from "./relations";
 import postgres from "postgres";
 
-const client = postgres(process.env.DATABASE_URL!, { prepare: false });
+const client = postgres(process.env.POSTGRES_URL!, { prepare: false });
 export const db = drizzle({
   client,
   schema: { ...schema, ...relations },
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: "./app/chat/lib/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.POSTGRES_URL!,
   },
 });
